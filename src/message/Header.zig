af35700a1f735517ca08bd13601897b3cf1f0b17
Sourcerer <git+commits@sourcery.zone>
2025-09-18 07:29:15 +0200

    refactor(Header): use optional parameters.

--- src/message/Header.zig
+++ src/message/Header.zig
@@ -16,21 +16,35 @@ ancount: u16,
 nscount: u16,
 arcount: u16,
 
-pub fn fromList(input: struct { u16, u1, u4, u1, u1, u1, u1, u3, u4, u16, u16, u16, u16 }) Header {
+pub fn init(
+    id: ?u16,
+    qr: ?u1,
+    opcode: ?u4,
+    aa: ?u1,
+    tc: ?u1,
+    rd: ?u1,
+    ra: ?u1,
+    z: ?u3,
+    rcode: ?u4,
+    qdcount: ?u16,
+    ancount: ?u16,
+    nscount: ?u16,
+    arcount: ?u16,
+) Header {
     return Header{
-        .id = input.@"0",
-        .qr = input.@"1",
-        .opcode = input.@"2",
-        .aa = input.@"3",
-        .tc = input.@"4",
-        .rd = input.@"5",
-        .ra = input.@"6",
-        .z = input.@"7",
-        .rcode = input.@"8",
-        .qdcount = input.@"9",
-        .ancount = input.@"10",
-        .nscount = input.@"11",
-        .arcount = input.@"12",
+        .id = id orelse 0,
+        .qr = qr orelse 0,
+        .opcode = opcode orelse 0,
+        .aa = aa orelse 0,
+        .tc = tc orelse 0,
+        .rd = rd orelse 0,
+        .ra = ra orelse 0,
+        .z = z orelse 0,
+        .rcode = rcode orelse 0,
+        .qdcount = qdcount orelse 0,
+        .ancount = ancount orelse 0,
+        .nscount = nscount orelse 0,
+        .arcount = arcount orelse 0,
     };
 }
 
@@ -59,8 +73,56 @@ pub fn toBytes(self: Header) [12]u8 {
     return result;
 }
 
+test "init: sets parameters" {
+    const H = Header.init(1, 1, 3, 1, 1, 1, 1, 7, 9, 10, 11, 12, 13);
+    try testing.expectEqual(1, H.id);
+    try testing.expectEqual(1, H.qr);
+    try testing.expectEqual(3, H.opcode);
+    try testing.expectEqual(1, H.aa);
+    try testing.expectEqual(1, H.tc);
+    try testing.expectEqual(1, H.rd);
+    try testing.expectEqual(1, H.ra);
+    try testing.expectEqual(7, H.z);
+    try testing.expectEqual(9, H.rcode);
+    try testing.expectEqual(10, H.qdcount);
+    try testing.expectEqual(11, H.ancount);
+    try testing.expectEqual(12, H.nscount);
+    try testing.expectEqual(13, H.arcount);
+}
+
+test "init: sets default value for null parameters" {
+    const H = Header.init(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+    );
+    try testing.expectEqual(0, H.id);
+    try testing.expectEqual(0, H.qr);
+    try testing.expectEqual(0, H.opcode);
+    try testing.expectEqual(0, H.aa);
+    try testing.expectEqual(0, H.tc);
+    try testing.expectEqual(0, H.rd);
+    try testing.expectEqual(0, H.ra);
+    try testing.expectEqual(0, H.z);
+    try testing.expectEqual(0, H.rcode);
+    try testing.expectEqual(0, H.qdcount);
+    try testing.expectEqual(0, H.ancount);
+    try testing.expectEqual(0, H.nscount);
+    try testing.expectEqual(0, H.arcount);
+}
+
 test "Header.toBytes sets id and qr" {
-    const header = Header.fromList(.{ 1234, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+    const header = Header.init(1234, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     const expected = [12]u8{ 0x4, 0xd2, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     try testing.expectEqual(expected, header.toBytes());
 }
